09e02216b356b0424abd58cad47e99fa92dfb15c
abisheksrikaanth <abishek@technicaliti.me>
2024-01-05 01:59:00 +0530

    More Code Fixes

--- src/Resource/Submissions.php
+++ src/Resource/Submissions.php
@@ -2,6 +2,7 @@
 
 namespace DocuSealCo\DocuSeal\Resource;
 
+use DocuSealCo\DocuSeal\Requests\Models\Message;
 use DocuSealCo\DocuSeal\Requests\Submissions\ArchiveSubmission;
 use DocuSealCo\DocuSeal\Requests\Submissions\CreateSubmission;
 use DocuSealCo\DocuSeal\Requests\Submissions\CreateSubmissionsFromEmails;
@@ -20,39 +21,27 @@ class Submissions extends Resource
      * @param  string|null  $templateFolder  Filter submissions by template folder name.
      * @param  int|null  $limit  The number of submissions to return. Default value is 10. Maximum value is 100.
      * @param  int|null  $before  The unique identifier of the submission to end the list with. Allows to receive only submissions with id less than the specified value.
+     * @param  int|null  $after  The unique identifier of the submission to start the list with. Allows to receive only submissions with id greater than the specified value.
      * @return Response
      * @throws FatalRequestException
      * @throws RequestException
      */
-	public function listAllSubmissions(
-		?int $templateId = null,
-		?string $applicationKey = null,
-		?string $templateFolder = null,
-		?int $limit = 10,
-		?int $before = null,
+    public function listAllSubmissions(
+        ?int $templateId = null,
+        ?string $applicationKey = null,
+        ?string $templateFolder = null,
+        ?int $limit = 10,
+        ?int $before = null,
         ?int $after = null,
-	): Response
-	{
-		return $this->connector->send(new ListAllSubmissions($templateId, $applicationKey, $templateFolder, $limit, $before, $after));
-	}
+    ): Response {
+        return $this->connector->send(new ListAllSubmissions($templateId, $applicationKey, $templateFolder, $limit, $before, $after));
+    }
 
 
-	public function createSubmission(): Response
-	{
-		return $this->connector->send(new CreateSubmission());
-	}
-
-
-    /**
-     * @param  int  $id  The unique identifier of the submission.
-     * @return Response
-     * @throws FatalRequestException
-     * @throws RequestException
-     */
-	public function getSubmission(int $id): Response
-	{
-		return $this->connector->send(new GetSubmission($id));
-	}
+    public function createSubmission(int $templateId, array $submitters, bool $sendEmail = true, bool $sendSms = false, string $order = 'preserved', ?Message $message = null): Response
+    {
+        return $this->connector->send(new CreateSubmission($templateId, $submitters, $sendEmail, $sendSms, $order, $message));
+    }
 
 
     /**
@@ -61,14 +50,26 @@ class Submissions extends Resource
      * @throws FatalRequestException
      * @throws RequestException
      */
-	public function archiveSubmission(int $id): Response
-	{
-		return $this->connector->send(new ArchiveSubmission($id));
-	}
+    public function getSubmission(int $id): Response
+    {
+        return $this->connector->send(new GetSubmission($id));
+    }
 
 
-	public function createSubmissionsFromEmails(): Response
-	{
-		return $this->connector->send(new CreateSubmissionsFromEmails());
-	}
+    /**
+     * @param  int  $id  The unique identifier of the submission.
+     * @return Response
+     * @throws FatalRequestException
+     * @throws RequestException
+     */
+    public function archiveSubmission(int $id): Response
+    {
+        return $this->connector->send(new ArchiveSubmission($id));
+    }
+
+
+    public function createSubmissionsFromEmails(int $templateId, array $emails, $sendEmail = true, ?Message $message = null): Response
+    {
+        return $this->connector->send(new CreateSubmissionsFromEmails($templateId, $emails, $sendEmail, $message));
+    }
 }
