114194496da5d2b16ca5f0385f4a63ee0c8401c4
Abishek R Srikaanth <1639302+abishekrsrikaanth@users.noreply.github.com>
2024-05-18 17:48:48 +0530

    Adding more properties to Submitter model

--- src/Models/Submitter.php
+++ src/Models/Submitter.php
@@ -4,6 +4,7 @@ namespace DocuSealCo\DocuSeal\Models;
 
 use DateTime;
 use DocuSealCo\DocuSeal\Casts\DateTime as DateTimeCast;
+use Spatie\LaravelData\Attributes\MapInputName;
 use Spatie\LaravelData\Attributes\WithCast;
 use Spatie\LaravelData\Data;
 use Spatie\LaravelData\Optional;
@@ -11,30 +12,64 @@ use Spatie\LaravelData\Optional;
 class Submitter extends Data
 {
     public int $id;
+
     public int $submission_id;
+
     public string $uuid;
+
     public string $email;
+
+    #[MapInputName('ua')]
+    public ?string $userAgent;
+
+    public ?string $ip;
+
     public string $slug;
+
     #[WithCast(DateTimeCast::class)]
     public ?DateTime $sent_at;
+
     #[WithCast(DateTimeCast::class)]
     public ?DateTime $opened_at;
+
     #[WithCast(DateTimeCast::class)]
     public ?DateTime $completed_at;
+
     #[WithCast(DateTimeCast::class)]
     public DateTime $created_at;
+
     #[WithCast(DateTimeCast::class)]
     public DateTime $updated_at;
+
     public string $name;
+
     public ?string $phone;
+
     public ?string $external_id;
+
     public array $metadata;
+
     public string $status;
+
     public ?string $application_key;
-    public array $values;
+
+    /** @var Value[] */
+    public array|Optional $values;
+
     public array $preferences;
+
     public string $role;
+
     public string|Optional $embed_src;
+
     /** @var Document[] */
     public array|Optional $documents;
+
+    public ?string $audit_log_url;
+
+    public ?string $submission_url;
+
+    public Template|Optional $template;
+
+    public Submission|Optional $submission;
 }
